bfbd1c320636f85925ffa068b207ae7b02193bc5
PR154357 <privera@popular-safi.com>
2024-02-21 14:49:42 -0500

    Initial Commit

---
+++ backend/sistemas/src/test/java/io/sistemas/ExampleTIIT.java
@@ -0,0 +1,8 @@
+package io.sistemas;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleTIIT extends ExampleTITest {
+    // Execute the same tests but in packaged mode.
+}
